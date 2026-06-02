ea18575a6fbf74e33cde02d4245dd322ae084465
ntnsergi <sergi.galarzaend@gmail.com>
2026-06-02 12:22:00 -0500

    v1

---
+++ base-datos/configuracion.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+
+# mysql
+# pip install mysql-connector-python
+# engine = create_engine("mysql+mysqlconnector://root:rootpassword@localhost:3308/user", echo=True)
+
+# postgres
+# sudo apt install libpq-dev
+# pip install psycopg2
+engine = create_engine("postgresql+psycopg2://user:password@localhost:5434/user", echo=True)
+s
+
+
---
+++ base-datos/crear_base_entidades.py
@@ -0,0 +1,52 @@
+from sqlalchemy import create_engine
+from configuracion import engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship 
+
+Base = declarative_base()
+
+from sqlalchemy import Column, Integer, String
+
+class Facultad(Base):
+    __tablename__ = 'facultad'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String, unique=True)
+    ubicacion = Column(String)
+    decano = Column(String)
+    
+    carreras = relationship('Carrera', back_populates='facultad')
+
+class Carrera(Base):
+    __tablename__ = 'carrera'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String)
+    codigo = Column(String, unique=True)
+    facultad_id = Column(Integer, ForeignKey('facultad.id'))
+    
+    facultad = relationship('Facultad', back_populates='carreras')
+    profesores = relationship('Profesor', back_populates='carrera')
+
+class Profesor(Base):
+    __tablename__ = 'profesor'
+    id = Column(Integer, primary_key=True)
+    nombres = Column(String)
+    apellidos = Column(String)
+    correo = Column(String, unique=True)
+    especialidad = Column(String)
+    carrera_id = Column(Integer, ForeignKey('carrera.id'))
+    
+    carrera = relationship('Carrera', back_populates='profesores')
+    recursos = relationship('RecursoAcademico', back_populates='profesor')
+
+class RecursoAcademico(Base):
+    __tablename__ = 'recurso_academico'
+    id = Column(Integer, primary_key=True)
+    titulo = Column(String)
+    fecha_publicacion = Column(Date)
+    tipo = Column(String)
+    url = Column(String)
+    profesor_id = Column(Integer, ForeignKey('profesor.id'))
+    
+    profesor = relationship('Profesor', back_populates='recursos')
+    
+Base.metadata.create_all(engine)
